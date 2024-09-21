213b5c9c5c2bc63fb1400e5a4d54df1045f29009
Sathish <sathish@sathishs-air.mynetworksettings.com>
2024-09-21 16:26:48 -0400

    email_functionality

--- script.js
+++ script.js
@@ -1,5 +1,68 @@
 let header = document.querySelector(".header-main");
 let navbar = document.querySelector(".navbar");
+let requestorName = document.getElementById("name");
+let requestorEmail = document.getElementById("email");
+let requestorMessage = document.getElementById("message");
+let requestorPhone = document.getElementById("phone");
+let sendEmailButton = document.getElementById("emailButton");
+let backdrop = document.getElementById("backdrop");
+
+function checkFields() {
+  if (
+    requestorName.value.trim() !== "" &&
+    requestorEmail.value.trim() !== "" &&
+    requestorMessage.value.trim() !== "" &&
+    requestorPhone.value.trim() !== ""
+  ) {
+    sendEmailButton.disabled = false; // Enable button if all fields are filled
+  } else {
+    sendEmailButton.disabled = true; // Disable button if any field is empty
+  }
+}
+// let allInputFields = [...document.querySelectorAll("input")];
+
+/* if (requestorName.length && requestorName.length && requestorMessage) {
+  sendEmailButton.disabled = false;
+} else {
+  sendEmailButton.disabled = false;
+} */
+
+requestorName.addEventListener("input", checkFields);
+requestorEmail.addEventListener("input", checkFields);
+requestorPhone.addEventListener("input", checkFields);
+requestorMessage.addEventListener("input", checkFields);
+
+/* function checkRequestorName() {
+  if (requestorName.value.trim() !== "") {
+    sendEmailButton.disabled = false; // Enable button if input is not empty
+  } else {
+    sendEmailButton.disabled = true; // Disable button if input is empty
+  }
+}
+
+function checkRequestorEmail() {
+  if (requestorEmail.value.trim() !== "") {
+    sendEmailButton.disabled = false; // Enable button if input is not empty
+  } else {
+    sendEmailButton.disabled = true; // Disable button if input is empty
+  }
+}
+
+function checkRequestorPhone() {
+  if (requestorPhone.value.trim() !== "") {
+    sendEmailButton.disabled = false; // Enable button if input is not empty
+  } else {
+    sendEmailButton.disabled = true; // Disable button if input is empty
+  }
+}
+
+function checkRequestorMessage() {
+  if (requestorMessage.value.trim() !== "") {
+    sendEmailButton.disabled = false; // Enable button if input is not empty
+  } else {
+    sendEmailButton.disabled = true; // Disable button if input is empty
+  }
+} */
 
 document.querySelector("#menu").onclick = () => {
   navbar.classList.toggle("active");
@@ -42,9 +105,35 @@ var swiper = new Swiper(".review-slider", {
 });
 
 function openForm() {
+  backdrop.style.display = "block";
   document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+  backdrop.style.display = "none";
+}
+
+//Email Functionality
+function sendEmail(event) {
+  event.preventDefault();
+  var params = {
+    from_name: requestorName.value,
+    email_id: requestorEmail.value,
+    message: requestorMessage.value,
+  };
+
+  emailjs
+    .send("service_29bj566", "template_5sawk7e", params)
+    .then((val) => {
+      alert("We have got your Registration Request!!!");
+      document.getElementById("myForm").style.display = "none";
+      requestorName.value = "";
+      requestorEmail.value = "";
+      requestorMessage.value = "";
+      requestorPhone.value = "";
+    })
+    .catch((err) => {
+      alert("Something Went Wrong!!! " + err);
+    });
 }
